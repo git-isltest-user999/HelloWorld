c46d9c8a96097460a49d058a01918d0f441957c8
Yamada Taro <yamada@taro>
2013-02-19 17:12:00 +0900

    ビルド成功

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,7 +8,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello, World!");
+            }
         }
     }
 }
