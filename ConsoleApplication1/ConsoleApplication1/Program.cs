b6495d35d104daf6a54823d45ee9e65def496c7e
SK <skita@isltest.local>
2013-02-26 10:06:48 +0900

    a

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,19 +8,6 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Int32 あ;
-            String エラーメッセージ = "エラーです";
-
-            if (Int32.TryParse("10", out あ))
-            {
-                Console.WriteLine(あ);
-            }
-            else
-            {
-                Console.WriteLine(エラーメッセージ);
-            }
-
-
             Console.Write("Exit");
         }
     }
